1596ba970926367804de0a0179fe5f65be00d479
Andrew Madigan <amadigan@users.noreply.github.com>
2024-12-21 13:51:31 -0800

    base project

---
+++ src/test/java/com/example/AppTest.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class AppTest {
+    @Test
+    void testApp() {
+        assertTrue(true, "Simple test should pass");
+    }
+}
